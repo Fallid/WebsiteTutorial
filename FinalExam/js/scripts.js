5ca0d9112745170c6d2e7713cc26767fa5dcd416
Fallid <naufalopal002@gmail.com>
2023-07-09 14:17:36 +0700

    question replacement

--- FinalExam/js/scripts.js
+++ FinalExam/js/scripts.js
@@ -79,46 +79,7 @@ var expire = setInterval(function () {
 
 
 //Question Display
-const quizData = [
-    {
-      number : 1,
-      question: "An entity can only have one Primary UID. True or False?",
-      correct: "a",
-      a: "True", 
-      b: "False",
-  
-    },
-    {
-      number: 2,
-      question: "If an entity has no attribute suitable to be a Primary UID, we can create an artificial one. True or False?",
-      correct: "a",
-      a: "True", 
-      b: "False",
-    },
-    {
-      number: 3,
-      question: "There is no limit to how many columns can make up an entity's UID. True or False?",
-      correct: "a",
-      a: "True", 
-      b: "False",
-    },
-    {
-      number: 4,
-      question: "A unique identifier can only be made up of one attribute. True or False?",
-      correct: "b",
-      a: "True", 
-      b: "False",
-    },
-    {
-      number: 5,
-      question: "When is an entity in 2nd Normal Form?",
-      correct: "a",
-      a:"When all non-UID attributes are dependent upon the entire UID.",
-      b: "When attributes with repeating or multi-values are present.",
-      c: "When no attritibutes are mutually independent and all are fully dependent on the primary key.",
-      d: "None of the Above.",
-    },
-  ];
+
   
 //Question Variabel
 const quiz = document.getElementById("quiz");
@@ -199,16 +160,16 @@ btnNext.addEventListener('click', () =>{
             score++;
             localStorage.setItem("score", score);
         }
-        currentQuiz++;
-        if(currentQuiz > quizData.length-1){
-            currentQuiz = quizData.length -1;
-        }
-        if(currentQuiz < quizData.length){
-            loadQuiz();
-        } else{
-            '<button onclick="location.reload()"> Reload </button>'
-        } 
     }
+    currentQuiz++;
+    if(currentQuiz > quizData.length-1){
+        currentQuiz = quizData.length -1;
+    }
+    if(currentQuiz < quizData.length){
+        loadQuiz();
+    } else{
+        '<button onclick="location.reload()"> Reload </button>'
+    } 
 })
 
 //for prev question btn
